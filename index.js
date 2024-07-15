36947a0dff04012f597238987c62612f71122d1b
TGOlivermou <164357313+TGOlivermou@users.noreply.github.com>
2024-07-15 04:34:41 -0300

    treinando

---
+++ index.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+
+    const envelope = $('#envelope');
+    const btn_open = $("#open");
+    const btn_reset = $("#reset");
+    
+    envelope.click( function() {
+        open();
+    });
+    btn_open.click( function() {
+        open();
+    });
+    btn_reset.click( function() {
+        close();
+    });
+
+    function open() {
+        envelope.addClass("open")
+           .removeClass("close");
+    }
+    function close() {
+        envelope.addClass("close")
+           .removeClass("open");
+    }
+
+});
